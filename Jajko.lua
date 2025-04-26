6df82b70fde433dfa65d7415dddb18ae7fe7e449
Jajko1377 <gggjh881@gmail.com>
2025-04-26 22:17:27 +0700

    Update Jajko.lua

--- Jajko.lua
+++ Jajko.lua
@@ -11,14 +11,14 @@ local Window = Rayfield:CreateWindow({
     },
     Discord = {
        Enabled = true,
-       Invite = "https://discord.gg/c7UfkMmnEs", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       Invite = "discord.gg/U3mYTmqdcB", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
     KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
        Title = "Jajko1337",
        Subtitle = "Key System",
-       Note = "https://discord.gg/c7UfkMmnEs",
+       Note = "discord.gg/U3mYTmqdcB",
        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
@@ -28,7 +28,7 @@ local Window = Rayfield:CreateWindow({
 
 Rayfield:Notify({
    Title = "Hub Jajko1337",
-   Content = "discord.gg/c7UfkMmnEs",
+   Content = "discord.gg/U3mYTmqdcB",
    Duration = 6.5,
    Image = "119578730668589",
 })
