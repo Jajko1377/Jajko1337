1304cce7b20a03719f613f86b9383eba6b636c5c
Jajko1377 <gggjh881@gmail.com>
2025-05-09 11:32:53 +0700

    Jajko hub script.lua

--- Jajko hub script.lua
+++ Jajko hub script.lua
@@ -1,3 +1,57 @@
 --Key - discord.gg/8jCD2cMFhs
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Jajko1377/Jajko1337/refs/heads/main/Jajko.lua"))()
+
+-- Script Jajko Hub
+
+-- IY
+
+-- TP
+
+-- Dex
+
+-- Other 80+ Game
+
+-- Fly
+
+-- Other
+
+-- MM2
+
+-- MM2 no key
+
+-- Shaders
+
+-- R6-R15 emotes
+
+-- Jerk off R6-R15
+
+-- SCP R6
+
+-- Knife V4
+
+-- Patchma
+
+-- Bird
+
+-- Fake murder
+
+-- R6 Penguin
+
+-- Genesis all script
+
+-- Aimlock
+
+-- ESP
+
+-- Gravity tool
+
+-- Invisible tool
+
+-- Tornado
+
+-- F3X tool
+
+-- Gun
+
+-- Screen stretching
