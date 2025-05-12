79fa040ba65e01fedc426ace5fcb56aa65dd5879
Jajko1377 <gggjh881@gmail.com>
2025-05-12 12:42:36 +0700

    Create 32.lua

---
+++ 32.lua
@@ -0,0 +1,7 @@
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local cameraMaxZoom = Value
+
+player.CameraMaxZoomDistance = cameraMaxZoom
+player.CameraMinZoomDistance = 0.5
